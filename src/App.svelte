6f0a967b6eda84e4ec6d2211e628ec7cbc5af378
Olavi Haapala <olavi.haapala@futurice.com>
2020-05-25 10:11:08 +0300

    Scale margins

--- src/App.svelte
+++ src/App.svelte
@@ -18,16 +18,19 @@
 	h1 {
 		text-transform: uppercase;
 		font-size: 8vw;
+		margin: 4vh auto;
 		font-weight: bold;
 	}
 
 	.weekDay {
 		font-size: 16vw;
+		margin: 8vh auto;
 		color:#ff3e00;
 	}
 
 	.displayDate {
 		font-size: 6vw;
+		margin: 3vh auto;
 	}
 
 	@media (min-width: 640px) {
